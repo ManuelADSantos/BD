49cb06626eea8a94440266f7558b4d5de325d67d
ManuelSantos <45367155+ManuelADSantos@users.noreply.github.com>
2020-12-02 22:52:26 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+# A função connect permite estabelecer uma ligação a uma base de dados
+# Verifique se a password é igual à que escolheu na instalação de PostgreSQL
+conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=postgres")
+
+# Cria um objecto (cursor) que permite executar operações sobre a base de dados
+cur = conn.cursor()
+
+# Efectua uma consulta à base de dados
+cur.execute("SELECT * FROM emp;")
+
+# mostra todos os resultados
+print(cur.fetchall())
+
+# Fecha a ligação à base de dados
+cur.close()
+conn.close()

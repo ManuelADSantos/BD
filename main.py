fe9dcc266d5ab6ac28e333546da35e3db3122bd4
ManuelSantos <ManuelSantos@users.noreply.github.com>
2020-12-04 22:57:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,55 +3,74 @@ import psycopg2.extras
 from passlib.hash import sha256_crypt
 from getpass import getpass
 
-
+#==========================================================================================================================
 # Estabelecer ligação à base de dados
 conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=postgres")
 
 # Criar cursor
 cur = conn.cursor()
-
-#Início=========================================================================
-while True:
-    try:
-        print("----------------------------Início--------------------------------")
-        print("\nBem vind@ ao NETFLOX\n    1 - Login\n    2 - Registar")
-        inicio_escolha = int(input("\nSelecione a opção que deseja: "))
-        if (inicio_escolha == 1 or inicio_escolha == 2):
-            break
-        else:
-            print("!!! Opção inválida !!!")
-    except ValueError:
-        print("!!! Opção inválida !!!")
-
-#Registo========================================================================
-if(inicio_escolha == 2):
-    #Registo do Email
+#==========================================================================================================================
+#Início
+def inicio():
     while True:
-        print("\n----------------------------Registo--------------------------------")
-        email = input("Insira o seu endereço de email: \n    ")
+        try:
+            print("----------------------------Início--------------------------------")
+            print("\nBem vind@ ao NETFLOX\n    1 - Login\n    2 - Registar")
+            inicio_escolha = int(input("\nSelecione a opção que deseja: "))
+            if (inicio_escolha == 1):       #Login
+                print("login")
+            elif(inicio_escolha == 2):      #Registo
+                registo()
+            else:
+                print("!!! Opção inválida !!!")
+        except ValueError:
+            print("!!! Opção inválida !!!")
+
+
+#==========================================================================================================================
+#Registo
+def registo():
+    #Registo do Email
+    print("\n----------------------------Registo--------------------------------")
+    while True:
+        email = input("\nInsira o seu endereço de email\n    ")
         cur.execute(f"SELECT COUNT(email) from utilizador where email like '%{email}'")
         email_verif = cur.fetchone()
-        if ("@" in email and email_verif[0]==0 and ' ' not in email):
-            print(f"Email inserido:\n    {email}")
+        if ("@" in email and email_verif[0]==0 and ' ' not in email and email[0]!= '@' and email[len(email)-1]!='@' and '.' in email):
+            print(f"Email inserido\n    {email}")
             break
         else:
             print("\nEndereço de email inválido")
 
     #Registo da Password
     while(True):
-        password = getpass("\nInsira a sua password:\n    ")
+        password = getpass("\n\nInsira a sua password\n    ")
         password_encriptada = sha256_crypt.hash(password)
-        password_verif = getpass("Confirme a sua password:\n    ")
+        password_verif = getpass("Confirme a sua password\n    ")
         if(sha256_crypt.verify(password_verif ,password_encriptada)):
-            print("\nPassword aceite")
+            print("Password aceite\n")
             break
         else:
-            print("\nPasswords não correspondem")
+            print("Passwords não correspondem")
 
     #Registo do nome
-    nickname = input("Insira o seu nome: \n    ")
-    print(f"Nome inserido:\n    {nickname}")
+    nickname = input("\n\nInsira o seu nome\n    ")
+    print(f"Nome inserido\n    {nickname}")
 
+    #De volta ao início
+    return
+
+#==========================================================================================================================
+#Procedimento
+inicio()
+
+
+
+
+
+
+
+#==========================================================================================================================
 # Fecha a ligação à base de dados
 cur.close()
 conn.close()

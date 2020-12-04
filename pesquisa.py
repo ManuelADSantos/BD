7c4dc2515c96a704dce11b90f71ae07eab37f09a
Matilde Carvalho <uc2019233490@users.noreply.github.com>
2020-12-04 00:38:01 +0000

    inputs a dar erro quando há mais que uma letra e na data nao dá

--- pesquisa.py
+++ pesquisa.py
@@ -1,15 +1,18 @@
-#PESQUISA
+# PESQUISA
 
 import psycopg2
 
-    # A função connect permite estabelecer uma ligação a uma base de dados
-    # Verifique se a password é igual à que escolheu na instalação de PostgreSQL
+# A função connect permite estabelecer uma ligação a uma base de dados
+# Verifique se a password é igual à que escolheu na instalação de PostgreSQL
+
 conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=postgres")
 
-    # Cria um objecto (cursor) que permite executar operações sobre a base de dados
+# Cria um objecto (cursor) que permite executar operações sobre a base de dados
 cur = conn.cursor()
 
-while (1):
+pesquisa = True
+
+while pesquisa:
 
     print("-------------------------------------Pesquisa:-----------------------------------------------")
     print("\n Como pretende pesquisar? \n")
@@ -23,69 +26,92 @@ while (1):
                           6 - Ano
                           7 - Voltar
 
-                         Pesquisa por: """)
+    Pesquisa por: """)
     print("\n")
 
-    if pesquisa == "1" :
+    if pesquisa == "1":
 
-            titulo1 = input("Pesquisa por tipo: \n")
+        titulo1 = input("Pesquisa por tipo: \n")
 
-            cur.execute("SELECT titulo from artigo where tipo = 'titulo1' ORDER by titulo ASC")
+        cur.execute("SELECT titulo from artigo where tipo = %s ORDER by titulo ASC", titulo1)
 
+        p_tipo = cur.fetchone()
+
+        while p_tipo is not None:
+            print(p_tipo)
             p_tipo = cur.fetchone()
-
-            while p_tipo is not None:
-                print(p_tipo)
-                p_tipo = cur.fetchone()
+        #conn.commit()
 
     elif pesquisa == "2":
 
-            titulo2 = input ("Pesquisa por título: \n")
+        titulo2 = input("Pesquisa por título: \n")
 
-            p_titulo = cur.execute("SELECT titulo from artigo where titulo = 'titulo2' ORDER by titulo ASC")
+        cur.execute("SELECT titulo from artigo where titulo = %s ORDER by titulo ASC", titulo2)
 
+        p_titulo = cur.fetchall()
+
+        while p_titulo is not None:
             print(p_titulo)
+            p_titulo = cur.fetchone()
 
-    elif pesquisa =="3":
+        #conn.commit()
 
-            titulo3 = input ("Pesquisa por Ator: \n")
+    elif pesquisa == "3":
 
-            p_atores = cur.execute("SELECT Titulo from Artigo where Ator = titulo3 ORDER by ASC")
+        titulo3 = str(input("Pesquisa por Ator: \n"))
 
-            print(p_atores)
+        p_atores = cur.execute("SELECT titulo from artigo where ator = %s  ORDER by titulo ASC", titulo3)
+
+        print(p_atores)
 
     elif pesquisa == "4":
 
-            titulo4 = input("Pesquisa por realizador: \n")
+        titulo4 = input(str("Pesquisa por realizador: \n"))
 
-            p_realizador = cur.execute("SELECT Titulo from Artigo where Realizador = ORDER by ASC")
+        cur.execute("SELECT titulo from artigo where realizador = %s ORDER by titulo ASC", str(titulo4))
 
+        p_realizador = cur.fetchall()
+
+        while p_realizador is not None:
             print(p_realizador)
+            p_realizador = cur.fetchone()
+        conn.commit()
 
     elif pesquisa == "5":
 
-            titulo5 = input("Pesquisa por produtor: \n")
+        titulo5 = input("Pesquisa por produtor: \n")
 
-            p_produtor = cur.execute("SELECT Titulo from Artigo where Produtor = titulo5 ORDER by ASC")
+        cur.execute("SELECT titulo from artigo where produtor = %s ORDER by titulo ASC", titulo5)
 
+        p_produtor = cur.fetchall()
+
+        while p_produtor is not None:
             print(p_produtor)
+            p_produtor = cur.fetchone()
+
+        conn.commit()
 
     elif pesquisa == "6":
 
-            titulo6 = input("Pesquisa por Ano: \n")
+        titulo6 = int(input("Pesquisa por Ano: \n"))
 
-            p_ano = cur.execute("SELECT Titulo from Artigo where Ano = titulo6 ORDER by ASC")
+        cur.execute("SELECT Titulo from Artigo where Ano = %s ORDER by titulo ASC", titulo6)
 
+        p_ano = cur.fetchall()
+
+        while p_ano is not None:
             print(p_ano)
+            p_ano = cur.fetchone()
+
+        conn.commit()
 
     elif pesquisa == "7":
 
-        break
+        pesquisa = False
 
     else:
-            print("Inválido")
-            print("Tenta outra vez")
-
+        print("Inválido")
+        print("Tenta outra vez")
 
 # Fecha a ligação à base de dados
 cur.close()

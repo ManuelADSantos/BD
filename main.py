57ca8ea80345febeebf0d873e5cf2a9f2e690e93
ManuelSantos <ManuelSantos@users.noreply.github.com>
2020-12-05 18:03:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def inicio():
             print("\nBem vind@ ao NETFLOX\n    1 - Login\n    2 - Registar")
             inicio_escolha = int(input("\nSelecione a opção que deseja: "))
             if (inicio_escolha == 1):       #Login
-                print("login")
+                login()
             elif(inicio_escolha == 2):      #Registo
                 registo()
             else:
@@ -53,35 +53,44 @@ def login():
 #==========================================================================================================================
 #Registo
 def registo():
-    #Registo do Email
-    print("\n----------------------------Registo--------------------------------")
     while True:
-        email = input("\nInsira o seu endereço de email\n    ")
-        cur.execute(f"SELECT COUNT(email) from utilizador where email like '%{email}'")
-        email_verif = cur.fetchone()
-        if ("@" in email and email_verif[0]==0 and ' ' not in email and email[0]!= '@' and email[len(email)-1]!='@' and '.' in email):
-            print(f"Email inserido\n    {email}")
-            break
-        else:
-            print("\nEndereço de email inválido")
+        #Registo do Email
+        print("\n----------------------------Registo--------------------------------")
+        while True:
+            email = input("\nInsira o seu endereço de email\n    ")
+            cur.execute(f"SELECT COUNT(email) from utilizador where email like '%{email}'")
+            email_verif = cur.fetchone()
+            if ("@" in email and email_verif[0]==0 and ' ' not in email and email[0]!= '@' and email[len(email)-1]!='@' and '.' in email):
+                print(f"Email inserido\n    {email}")
+                break
+            else:
+                print("\nEndereço de email inválido")
 
-    #Registo da Password
-    while(True):
-        password = getpass("\n\nInsira a sua password\n    ")
-        password_encriptada = sha256_crypt.hash(password)
-        password_verif = getpass("Confirme a sua password\n    ")
-        if(sha256_crypt.verify(password_verif ,password_encriptada)):
-            print("Password aceite\n")
-            break
-        else:
-            print("Passwords não correspondem")
+        #Registo da Password
+        while(True):
+            password = getpass("\n\nInsira a sua password\n    ")
+            password_encriptada = sha256_crypt.hash(password)
+            password_verif = getpass("Confirme a sua password\n    ")
+            if(sha256_crypt.verify(password_verif ,password_encriptada)):
+                print("Password aceite\n")
+                break
+            else:
+                print("Passwords não correspondem")
 
-    #Registo do nome
-    nickname = input("\n\nInsira o seu nome\n    ")
-    print(f"Nome inserido\n    {nickname}")
+        #Registo do nome
+        nickname = input("\n\nInsira o seu nome\n    ")
+        print(f"Nome inserido\n    {nickname}")
 
-    #De volta ao início
-    return
+        #Guardar dados na base de dados
+        try:
+            cur.execute(f"INSERT INTO utilizador(id, email, password, nome) VALUES (DEFAULT, '{email}', '{password_encriptada}', '{nickname}');")
+            #Confirmar mudanças
+            conn.commit()
+            #De volta ao início
+            print("BEM VINDO AO NETFLOX")
+            return
+        except:
+            print("Dados não válidos")
 
 
 #==========================================================================================================================

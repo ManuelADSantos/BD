f4952f3ef402b2ce9b7245346ad34b5a0de06e39
ManuelSantos <ManuelSantos@users.noreply.github.com>
2020-12-08 03:43:59 +0000

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,14 @@ def inicio():
             inicio_escolha = int(input("\n\t\t\t     "))
             if (inicio_escolha == 1):       #Login
                 login()
-                menu_admin()
+                #Diferenciar entre admin e cliente
+                cur.execute(f"SELECT * FROM administrador where utilizador_id = {utilizador_atual}")
+                ser_admin = cur.fetchone()
+                if ser_admin is not None:       #ADMIN
+                    menu_admin()
+                elif ser_admin is None:         #CLIENTE
+                    print("MENU CLIENTE")
+
             elif(inicio_escolha == 2):      #Registo
                 registo()
             elif(inicio_escolha == 3):      #Sair
@@ -69,6 +76,9 @@ def login():
                 password_verif = cur.fetchone()
                 if(sha256_crypt.verify(password ,password_verif[0])):
                     print("\nInicio de sessão bem sucedida\n")
+                    cur.execute(f"SELECT id FROM utilizador WHERE email LIKE '%{email}'")
+                    global utilizador_atual
+                    utilizador_atual = cur.fetchone()[0]
                     break
                 else:
                     print("\nPassword errada")
@@ -135,6 +145,7 @@ def menu_admin():
 
         escolha_admin = input("""
                               1 - Estatísticas
+                              2 - Adicionar Artigo
                               V|v- Logout
 
         Ver: """)
@@ -142,12 +153,133 @@ def menu_admin():
         if escolha_admin == "1":
             admin_estatisticas()
 
+        elif escolha_admin == "2":
+            admin_adicionarartigo()
+
         elif escolha_admin == "V" or escolha_admin== "v":
             print("LOGOUT")
             return
 
         else:
             print("Inválido\nTenta outra vez")
+
+
+#==========================================================================================================================
+#Menu ADMIN - Adicionar Artigos
+def admin_adicionarartigo():
+    while True:
+        print("\n-------------------------------------Adicionar Artigo-----------------------------------------------\n")
+
+        #Título
+        while True:
+            novo_titulo = input("Titulo do novo artigo: ")
+            validar = input("\nConfirma o titulo do artigo a introduzir? (S/N)\n")
+            if validar == "S" or validar == "s":
+                break
+            if validar == "N" or validar == "n":
+                print("\nTitulo descartado")
+            else:
+                print("\nOpção inválida")
+
+        #Tipo
+        while True:
+            novo_tipo = input("Tipo de artigo\n1 - Filme\n2 - Documentário\n3 - Série\n")
+            if novo_tipo == "1" or novo_tipo == "2" or novo_tipo == "3":
+                validar = input("Confirma o tipo do artigo a introduzir? (S/N)\n")
+                if validar == "S" or validar == "s":
+                    if novo_tipo == "1":
+                        novo_tipo = "filme"
+                    elif novo_tipo == "2":
+                        novo_tipo = "documentario"
+                    elif novo_tipo == "3":
+                        novo_tipo = "serie"
+                    break
+                if validar == "N" or validar == "n":
+                    print("\nTipo descartado")
+                else:
+                    print("\nOpção inválida")
+            else:
+                print("\nOpção inválida")
+
+        #Realizador
+        while True:
+            novo_realizador = input("Realizador do novo artigo: ")
+            validar = input("\nConfirma o realizador do artigo a introduzir? (S/N)\n")
+            if validar == "S" or validar == "s":
+                break
+            if validar == "N" or validar == "n":
+                print("\nRealizador descartado")
+            else:
+                print("\nOpção inválida")
+
+        #Produtor
+        while True:
+            novo_produtor = input("Produtor do novo artigo: ")
+            validar = input("\nConfirma o produtor do artigo a introduzir? (S/N)\n")
+            if validar == "S" or validar == "s":
+                break
+            if validar == "N" or validar == "n":
+                print("\nProdutor descartado")
+            else:
+                print("\nOpção inválida")
+
+        #Ano
+        while True:
+            novo_ano = int(input("Ano do novo artigo: "))
+            validar = input("\nConfirma o ano do artigo a introduzir? (S/N)\n")
+            if validar == "S" or validar == "s":
+                break
+            if validar == "N" or validar == "n":
+                print("\nAno descartado")
+            else:
+                print("\nOpção inválida")
+
+        #Periodo de aluguer
+        while True:
+            novo_periodoaluguer = int(input("Especifique o periodo de aluguer do novo artigo (em meses): "))
+            validar = input("\nConfirma o periodo de aluguer do artigo a introduzir? (S/N)\n")
+            if validar == "S" or validar == "s":
+                break
+            if validar == "N" or validar == "n":
+                print("\nPeriodo de aluguer descartado")
+            else:
+                print("\nOpção inválida")
+
+        #Efetuar Registo
+        try:
+            cur.execute(f"INSERT INTO artigo(id, titulo, tipo, realizador, produtor, ano, periodo_de_aluguer) VALUES (DEFAULT, '{novo_titulo}', '{novo_tipo}', '{novo_realizador}', '{novo_produtor}', {novo_ano}, {novo_periodoaluguer}) RETURNING id;")
+            id_artigo = cur.fetchone()[0]
+            perguntar = input("Pretende associar atores a este artigo? (S/N)\n")
+            if perguntar == "S" or perguntar == "s":
+                while True:
+                    ator = input("\nInsira o nome do ator ou atriz a associar a este artigo(Primeiro e Último nome): ")
+                    cur.execute(f"SELECT id FROM atores WHERE nome LIKE '%{ator}'")
+                    id_ator = cur.fetchone()[0]
+                    if id_ator is not None:         #Ator pertence à base de dados
+                        cur.execute(f"INSERT INTO artigo_atores(artigo_id, atores_id) VALUES ({id_artigo},{id_ator});")
+                        print(f"\n{ator} participa neste artigo")
+                        mais = input("Pretende adicionar mais atores?(S/N):\n")
+                        if mais == "S" or mais == "s":
+                            print("")
+                        else:
+                            break
+                    else:                           #Ator não pertence à base de dados
+                        print("Ator/atriz não registado/a na base de dados")
+                        mais = input("Pretende adicionar mais atores?(S/N):\n")
+                        if mais == "S" or mais == "s":
+                            print("")
+                        else:
+                            break
+
+                conn.commit()
+                return
+
+            else:
+                conn.commit()
+                return
+        except:
+            print("\nDados Inválidos")
+
 
 
 #==========================================================================================================================
@@ -191,59 +323,43 @@ def admin_estatisticas():
 
         #------------------------------------------Numero de artigos por tipo-----------------------------------------------
         elif estatisticas == "3":
-
-            cur.execute("SELECT count(tipo) from artigo where tipo like 'filme';")
-
-            print("Número de Filmes:" )
-
+            cur.execute("SELECT count(tipo) from artigo where tipo like 'filme';")          #Filmes
             nartigos_filme = cur.fetchone()
 
             if nartigos_filme is None:
-                print("Resultado não encontrado!")
+                print("Número de Filmes: 0")
+            elif nartigos_filme is not None:
+                print("Número de Filmes: ", *nartigos_filme)
 
-            while nartigos_filme is not None:
-                print("->", *nartigos_filme)
-                nartigos_filme = cur.fetchone()
 
-            cur.execute("SELECT count(tipo) from artigo where tipo like 'serie';")
-
-            print("Número de Séries:")
-
+            cur.execute("SELECT count(tipo) from artigo where tipo like 'serie';")          #Séries
             nartigos_serie = cur.fetchone()
 
             if nartigos_serie is None:
-                print("Resultado não encontrado!")
+                print("Número de Séries: 0")
+            elif nartigos_serie is not None:
+                print("Número de Séries: ", *nartigos_serie)
 
-            while nartigos_serie is not None:
-                print("->", *nartigos_serie)
-                nartigos_serie = cur.fetchone()
 
-            cur.execute("SELECT count(tipo) from artigo where tipo like 'documentario';")
-
-            print("Número de Documentários:")
-
+            cur.execute("SELECT count(tipo) from artigo where tipo like 'documentario';")   #Documentários
             nartigos_doc = cur.fetchone()
 
             if nartigos_doc is None:
-                print("Resultado não encontrado!")
+                print("Número de Documentários: 0")
+            elif nartigos_doc is not None:
+                print("Número de Documentários: ", *nartigos_doc)
 
-            while nartigos_doc is not None:
-                print("->", *nartigos_doc)
-                nartigos_doc = cur.fetchone()
 
         #-----------------------------------Valor total dos artigos alugados no momento atual-------------------------------
         elif estatisticas == "4":
-            cur.execute("SELECT sum(historico_precos.preco) from historico_precos join artigo on historico_precos.artigo_id = artigo.id join aluguer on artigo.id = aluguer.artigo_id where historico_precos.atual = True AND aluguer.ativo = True;")
-
-            print("Valor total dos Artigos alugados atualmente:")
+            cur.execute("SELECT sum(historico_precos.preco) FROM historico_precos JOIN artigo ON historico_precos.artigo_id = artigo.id JOIN aluguer ON artigo.id = aluguer.artigo_id WHERE historico_precos.atual = True AND aluguer.ativo = True;")
             valortotal = cur.fetchone()
 
-            if valortotal is None:
-                print("Resultado não encontrado!")
+            if valortotal[0] is None:
+                print("Valor total dos Artigos alugados atualmente: 0€")
+            elif valortotal[0] is not None:
+                print("Valor total dos Artigos alugados atualmente: ", *valortotal,"€")
 
-            while valortotal is not None:
-                print("->", *valortotal)
-                valortotal = cur.fetchone()
 
         #--------------------------------------Valor total dos alugueres desde sempre---------------------------------------
         elif estatisticas == "5":
@@ -295,7 +411,7 @@ def admin_estatisticas():
             return
 
         else:
-            print("Inválido\nTenta outra vez")
+            print("Inválido\nTente outra vez")
 
 #==========================================================================================================================
 #Iniciar programa

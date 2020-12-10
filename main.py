9cea954e44f638ea7c32b6134c16fc6e45b7953f
ManuelSantos <ManuelSantos@users.noreply.github.com>
2020-12-10 03:48:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -150,6 +150,7 @@ def menu_admin():
                               2 - Adicionar Artigo
                               3 - Remover Artigo
                               4 - Enviar Mensagens
+                              5 - Inventário de Artigos(detalhes e histórico de preços)
                               V|v- Logout
 
         Ver: """)
@@ -166,12 +167,163 @@ def menu_admin():
         elif escolha_admin == "4":
             admin_mensagens()
 
+        elif escolha_admin == "5":
+            inventario()
+
         elif escolha_admin == "V" or escolha_admin== "v":
             print("LOGOUT")
             return
 
         else:
             print("Inválido\nTenta outra vez")
+
+#==========================================================================================================================
+#Menu ADMIN - Inventário, detalhes e condições de aluguer
+def inventario():
+    while True:
+        print("---------------------------------INVENTÁRIO DE ARTIGOS----------------------------------------")
+
+        cur.execute("SELECT id, titulo, tipo from artigo ORDER BY tipo ASC, titulo ASC;")
+
+        for linha in cur.fetchall():
+            id, titulo, tipo = linha
+            print("Título: ", titulo,"| Tipo: ", tipo,"| ID: ", id)
+
+
+
+        print("\n\t\t\t\t   O que pretende fazer?")
+
+        det = input("\n\t\t\t\t1 - Ver Detalhes do Artigo\n\t\t\t\t\tV|v - Voltar\n\n\t\t\t\t\t    ")
+
+        # ----------------------------------------- VER DETALHES ARTIGO-------------------------------------------------
+        if det == "1":
+            while True:
+                print("\n---------------------------VER DETALHES ARTIGO---------------------------\n")
+                voltar = input("ENTER - Ver detalhes\nV/v - Voltar ao Inventário\n\n")
+                if voltar == "":
+                    while True:
+                        try:
+                            artdet = int(input("De que artigo quer ver detalhes?(ID): "));
+                            break
+                        except ValueError:
+                            print("Valor de ID inválido")
+
+
+                    try:
+                        cur.execute(f"SELECT titulo, tipo, realizador, produtor, ano from artigo where id = '{artdet}';")
+                        detalhes = cur.fetchone()
+                        if detalhes is None:
+                           print(f"Não existe um artigo com o ID {artdet}")
+                           break
+                        else:
+                            while detalhes is not None:
+                                print("\nDetalhes do artigo: ")
+                                print("[ Título | Tipo | Realizador | Produtor | Ano | Ator ]")
+                                print(detalhes)
+                                detalhes = cur.fetchone()
+
+                            print("\n Mais detalhes: \n")
+                            while True:
+
+                                det1 = input("""
+                                            1- Histórico de preços
+                                            2- Condições de Aluguer
+                                            V|v - Voltar
+                                VER: """)
+
+                                #Histórico de preços
+                                if det1 == "1":
+                                    historico(artdet)
+                                    break
+
+                                #Condições de aluguer
+                                elif det1 == "2":
+
+                                    print("\n----------------------------Condições de aluguer--------------------------------------\n")
+
+                                    cur.execute(f"SELECT periodo_de_aluguer, preco from artigo, historico_precos where artigo.id = {artdet} and historico_precos.atual = True and historico_precos.artigo_id = {artdet};")
+
+                                    for linha in cur.fetchall():
+                                        periodo_de_aluguer, preco = linha
+                                        print("Período de aluguer(em meses): ", periodo_de_aluguer, "| Preço: ", preco, " €")
+                                    break
+
+                                #Voltar
+                                elif det1 == "V" or det1 == "v":
+                                    break
+                                else:
+                                    print("Inválido")
+                                    print("Tente outra vez")
+                    except ValueError:
+                        print("ID inválido")
+
+                elif voltar == "v" or voltar == "V":
+                    print("A VOLTAR AO INVENTÁRIO")
+                    break
+                else:
+                    print("")
+
+        # Volta ao Inventário
+        elif det == "V" or det == "v":
+            break
+
+        # Input não válido
+        else:
+            print("\n\t\t\t\t\tInválido")
+            print("\t\t\t\t     Tenta outra vez\n")
+
+#==========================================================================================================================
+#Menu ADMIN - Histórico de preços
+def historico(art):
+
+    print("\n--------------------------------Histórico de preços----------------------------------------------\n")
+
+    cur.execute(f"SELECT preco, entrada_em_vigor, atual from historico_precos where artigo_id = '{art}' ORDER BY entrada_em_vigor DESC;")
+
+    for linha in cur.fetchall():
+        preco, entrada_em_vigor, atual = linha
+        print("Preço: ", preco, "€ | Entrada em vigor: ", entrada_em_vigor, " | Atual?:", atual)
+
+    while True:
+        corrigir = input("\nCorrigir Preço? (S|N): \n")
+
+        if corrigir == "S" or corrigir == "s":
+
+            print("--------------------------------------CORRIGIR PREÇO---------------------------------------------------")
+
+            while True:
+                try:
+                    p = float(input("Preço Novo: \n"))
+                    if p < 0:
+                        raise ValueError("INSIRA UM VALOR VÁLIDO!")
+                    else:
+                        break
+                except ValueError:
+                    print("INSIRA UM VALOR VÁLIDO!")
+
+            tentativas = 3
+            while tentativas > 0:
+                confirmar = getpass("Introduza a sua chave de administrador para confirmar a alteração do preço:\n")
+                cur.execute(f"SELECT chave FROM administrador WHERE utilizador_id = {utilizador_atual}")
+                chave = cur.fetchone()[0]
+                if confirmar == chave:
+                    cur.execute(f"SELECT mudar_estado({art});");
+                    cur.execute(f"INSERT INTO historico_precos(id,preco, entrada_em_vigor, atual, artigo_id) VALUES (DEFAULT,{p}, CURRENT_TIMESTAMP, True, {art});")
+                    print("Preço Alterado!")
+                    conn.commit()
+                    break
+                else:
+                    tentativas -= 1
+                    print(f"\nChave errada. Tem {tentativas} tentativas restantes")
+                    if tentativas == 0:
+                        print("\nAlteração de preço cancelada!")
+                        break
+
+            break
+
+        elif corrigir == "N" or corrigir == "n":
+            break
+
 
 #==========================================================================================================================
 #Menu ADMIN - Mensagens
@@ -205,10 +357,10 @@ def admin_mensagens():
                                 #Registar mensagem na tabela mensagem_administrador
                                 cur.execute(f"INSERT INTO mensagem_administrador(mensagem_id, administrador_utilizador_id) VALUES ({id_mensagem}, {utilizador_atual});")
                                 #Registar mensagem na tabela cliente_mensagem
-                                cur.execute("SELECT * FROM cliente")
+                                cur.execute("SELECT utilizador_id FROM cliente")
                                 for linha in cur.fetchall():
-                                    cur.execute(f"INSERT INTO cliente_mensagem(mensagem_id, cliente_utilizador_id) VALUES ({id_mensagem}, {linha['utilizador_id']});")
-
+                                    utilizador_id = linha[0]
+                                    cur.execute(f"INSERT INTO cliente_mensagem(mensagem_id, cliente_utilizador_id) VALUES ({id_mensagem}, {utilizador_id});")
                                 tentativas = 3
                                 sair = False
                                 while tentativas > 0:
@@ -460,7 +612,10 @@ def admin_adicionarartigo():
                         mais = input("Pretende adicionar mais atores ao artigo?(S/N):\n")
                         if mais == "S" or mais == "s":
                             print("")
+                        elif mais == "N" or mais == "n":
+                            break
                         else:
+                            print("Não válido")
                             break
                     elif id_ator is None:                           #Ator não pertence à base de dados
                         while True:
@@ -703,7 +858,7 @@ def admin_estatisticas():
         #--------------------------------------Valor total dos alugueres desde sempre---------------------------------------
         elif estatisticas == "5":
 
-            cur.execute("SELECT sum(preco) from historico_precos;")
+            cur.execute("SELECT sum(historico_precos.preco) FROM historico_precos JOIN artigo ON historico_precos.artigo_id = artigo.id JOIN aluguer ON artigo.id = aluguer.artigo_id WHERE historico_precos.atual = True;")
 
             print("Valor total dos Alugueres desde sempre:")
             alugueres = cur.fetchone()
@@ -712,7 +867,7 @@ def admin_estatisticas():
                 print("Resultado não encontrado!")
 
             while alugueres is not None:
-                print("->", *alugueres)
+                print("->", *alugueres,"€")
                 alugueres = cur.fetchone()
 
         #------------------------------------------Cliente com mais alugueres-----------------------------------------------

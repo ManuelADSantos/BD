8c2d3f554561a29cde70339303c498c75e4f372f
ManuelSantos <ManuelSantos@users.noreply.github.com>
2020-12-11 23:29:23 +0000

    Update saldos.py

--- saldos.py
+++ saldos.py
@@ -1,6 +1,9 @@
 #SALDOS
 
 import psycopg2
+from getpass import getpass
+
+utilizador_atual = 1
 
 # A função connect permite estabelecer uma ligação a uma base de dados
 # Verifique se a password é igual à que escolheu na instalação de PostgreSQL
@@ -141,10 +144,76 @@ def saldos_admin():
 #cur.execute("ALTER TABLE cliente ALTER COLUMN pedido_saldo DROP DEFAULT;")
 
 #---------------------------------------ADICIONAR SALDO ADMIN-------------------------------------------------------
+def alterarsaldo_admin():
+    while True:
+        print("------------------------------- ALTERAR SALDO -------------------------------\n")
+        cur.execute("SELECT utilizador_id, nome, saldo FROM cliente, utilizador WHERE cliente.utilizador_id = utilizador.id;")
+        dados = cur.fetchall()
+        if len(dados) == 0:
+            print("\n\t\t   Não existem clientes registados\n\n\t\t   || A VOLTAR AO MENU ||")
+            break
+        else:
+            for ind in range(len(dados)):
+                print("\t\t  ID: ", dados[ind][0], " |Nome: ", dados[ind][1], "|Saldo: ", dados[ind][2],"€")
 
-saldos_cliente()
 
-saldos_admin()
+            cliente_escolhido = input("\n\t\t\tID - Cliente a alterar o saldo\n\t\t\t     v/V - Voltar ao Menu\n\t\t\t\t       ")
+
+            #Voltar ao menu principal
+            if (cliente_escolhido == "v" or cliente_escolhido == "V"):
+                print("De volta ao MENU")
+                break
+            try:
+                #Verificar se ID existe
+                teste = int(cliente_escolhido)
+                if isinstance(teste, int):
+                    cur.execute(f"SELECT nome, saldo FROM utilizador, cliente WHERE utilizador_id = {cliente_escolhido}")
+                    verif = cur.fetchone()
+                    if verif is not None:
+                        saldo_atual = verif[1]
+                        try:
+                            valor = -1
+                            while valor < 0:
+                                valor = float(input("Indique o valor a aumentar no saldo: "))
+                                if valor < 0:
+                                    print("O valor deve ser positivo")
+                                else:
+                                    try:
+                                        saldo_novo = saldo_atual + valor
+                                        tentativas = 3
+                                        while tentativas > 0:
+                                            confirmar = getpass("Introduza a sua chave de administrador para confirmar a alteração do saldo:\n")
+                                            cur.execute(f"SELECT chave FROM administrador WHERE utilizador_id = {utilizador_atual}")
+                                            chave = cur.fetchone()[0]
+                                            cur.execute(f"UPDATE cliente SET saldo = {saldo_novo} WHERE utilizador_id = {cliente_escolhido}")
+                                            if confirmar == chave:
+                                                print("\nSaldo alterado com sucesso")
+                                                conn.commit()
+                                                break
+                                            else:
+                                                tentativas -= 1
+                                                print(f"\nChave errada. Tem {tentativas} tentativas restantes")
+                                                if tentativas == 0:
+                                                    print("\nAlteração de saldo cancelada!")
+                                                    conn.rollback()
+                                                    break
+                                    except:
+                                        print("ERRO NA INSERÇÃO")
+                        except ValueError:
+                            print("VALOR INVÁLIDO")
+                    else:
+                        print("NÃO EXISTE UM CLIENTE CUJO ID É O INSERIDO")
+            except:
+                #Valores não numéricos não são avaliados
+                print("VALOR INVÁLIDO")
+
+
+#----------------------------------------CHAMADA DAS FUNÇÕES--------------------------------------------------------
+#saldos_cliente()
+
+#saldos_admin()
+
+alterarsaldo_admin()
 
 # Fecha a ligação à base de dados
 cur.close()

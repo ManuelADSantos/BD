7d417fd69b13029add75b65b1a5b7695f13f5579
ManuelSantos <ManuelSantos@users.noreply.github.com>
2020-12-12 11:48:39 +0000

    Update main.py

--- main.py
+++ main.py
@@ -173,7 +173,7 @@ def menu_cliente():
             lista_cliente()
 
         if escolha_cliente == "6":
-            lista_cliente()
+            alugar_cliente()
 
         elif escolha_cliente == "V" or escolha_cliente == "v":
             print("LOGOUT")
@@ -858,7 +858,87 @@ def lista_cliente():
             print("\t\t\t\t     Tenta outra vez\n")
 
 
-#-------------------------------------Catálogo/ Lista de todos os artigos-------------------------------------------------
+#----------------------------------------Alugar artigos----------------------------------------------------
+def alugar_cliente():
+    while True:
+        print("---------------------------------ARTIGOS PARA ALUGAR----------------------------------------")
+        cur.execute(f"SELECT id, titulo, tipo FROM artigo EXCEPT SELECT artigo.id, titulo, tipo FROM artigo, aluguer WHERE artigo.id = aluguer.artigo_id AND ativo = true AND aluguer.cliente_utilizador_id = {utilizador_atual};")
+        for linha in cur.fetchall():
+            id, titulo, tipo = linha
+            print("Título: ", titulo, "| Tipo: ", tipo, "| ID: ", id)
+
+        artdet = input("\n\t\t\t         ID - Artigo a alugar\n\t\t\t         v/V - Voltar ao Menu\n\t\t\t\t       ")
+
+        if (artdet == "v" or artdet == "V"):
+            print("De volta ao MENU")
+            return
+
+        try:
+            artdet = int(artdet)
+            try:
+                cur.execute(f"SELECT titulo, tipo, realizador, produtor, ano from artigo where id = '{artdet}';")
+                detalhes = cur.fetchone()
+                if detalhes is None:
+                    print(f"Não existe um artigo com o ID {artdet}")
+                else:
+                    print("\nDetalhes do artigo: ")
+                    print(
+                        f"Título: {detalhes[0]} | Tipo: {detalhes[1]} | Realizador: {detalhes[2]} | Produtor: {detalhes[3]} | Ano: {detalhes[4]} ")
+
+                    try:
+                        cur.execute(f"SELECT nome FROM atores, artigo_atores WHERE atores.id = artigo_atores.atores_id and artigo_id = {artdet};")
+                        nomes = cur.fetchone()
+                        if nomes is None:
+                            print("Atores : N/A")
+                        else:
+                            print(f"Atores: ")
+                            while nomes is not None:
+                                print(" ", *nomes)
+                                nomes = cur.fetchone()
+                    except:
+                        print("Atores : N/A")
+
+                    print("\n----------------------------Condições de aluguer--------------------------------------\n")
+
+                    cur.execute(f"SELECT periodo_de_aluguer, preco from artigo, historico_precos where artigo.id = {artdet} and historico_precos.atual = True and historico_precos.artigo_id = {artdet};")
+
+                    for linha in cur.fetchall():
+                        periodo_de_aluguer, preco = linha
+                        print("Período de aluguer(em meses): ", periodo_de_aluguer, "| Preço: ", preco, " €")
+
+                    while True:
+
+                        det1 = input("""Pretende ALUGAR?
+
+                        Responda (S|N): """)
+
+                        if det1 == "S" or det1 == "s":
+                            cur.execute(f"SELECT saldo FROM cliente WHERE utilizador_id = {utilizador_atual}")
+                            saldo_atual = cur.fetchone()[0]
+
+                            if saldo_atual < preco:
+                                print("\nSALDO INSUFICIENTE PARA ALUGAR ESTE ARTIGO")
+                                break
+                            else:
+                                try:
+                                    cur.execute(f"INSERT INTO aluguer (id, data, ativo, artigo_id, cliente_utilizador_id, preco_aluguer) VALUES (DEFAULT, CURRENT_TIMESTAMP, True, {artdet}, {utilizador_atual},{preco});")
+                                    conn.commit()
+                                    print("\nALUGADO!\n")
+                                    break
+                                except:
+                                    conn.rollback()
+                                    print("\nALUGUER CANCELADO!\n")
+                                    break
+
+                        elif det1 == "N" or det1 == "n":
+                            break
+                        else:
+                            print("Inválido\nTenta outra vez")
+
+            except:
+                print("ERRO!")
+        except ValueError:
+            print("VALOR INVÁLIDO")
 
 #/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
 #                                                               ADMIN
